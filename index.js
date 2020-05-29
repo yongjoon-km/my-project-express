e1fc66c2a4ee6fdd0efaf5dbe479b1396e1ea173
yongkim <penubokim@gmail.com>
2020-05-29 22:40:25 +0900

    make api job request and create new database project db and jobs table

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '1234',
-	database: 'pets'
+	database: 'project_db'
 });
 
 connection.connect();
@@ -15,12 +15,15 @@ const port = 5000;
 
 app.get('/', (req, res) => {
 
-	var data;
-	connection.query('SELECT * FROM cats', (err, rows) => {
+	res.send('hello world');
+});
+
+app.get('/api/jobs', (req, res) => {
+	connection.query('SELECT * FROM jobs ORDER BY job_id', (err, rows) => {
 		if (err) throw err;
 
 		res.send(rows);
 	})
-});
+})
 
 app.listen(port, () => console.log(`listening... {port}`));
